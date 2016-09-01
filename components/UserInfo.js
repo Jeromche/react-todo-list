3159b6ee9cf7f0adb3d824e5c251f302870dcd91
Jeroen van der Tuin <jeroen@vandertuin.nl>
2016-09-01 15:16:56 -0500

    Combine reducers.

---
+++ components/UserInfo.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class UserInfo extends Component {
+  handleNewId() {
+    this.props.createNewUserId()
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Username: {this.props.user.username}</div>
+        <div>ID: {this.props.user.id}</div>
+        <button onClick={this.handleNewId.bind(this)}>
+          Random ID
+        </button>
+      </div>
+    )
+  }
+}
